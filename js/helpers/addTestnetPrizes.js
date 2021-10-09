692873177a623f52682543d6037dd605f86743b2
Jeevan Pillay <jeevanpillay@protonmail.com>
2021-10-09 16:34:23 +1100

    added add testnet prize wrappr

--- js/helpers/addTestnetPrizes.js
+++ js/helpers/addTestnetPrizes.js
@@ -1,64 +1,67 @@
-const hardhat = require('hardhat')
-const chalk = require("chalk")
-const debug = require('debug')('ptv3:addTestnetPrizes')
+const hardhat = require("hardhat");
+const chalk = require("chalk");
+const debug = require("debug")("ptv3:addTestnetPrizes");
 
 function dim() {
-  console.log(chalk.dim.call(chalk, ...arguments))
+  console.log(chalk.dim.call(chalk, ...arguments));
 }
 
 function green() {
-  console.log(chalk.green.call(chalk, ...arguments))
+  console.log(chalk.green.call(chalk, ...arguments));
 }
 
 async function addTestnetPrizes(prizePoolContractAddress, numberOfPrizes) {
-  const { getNamedAccounts, deployments, ethers } = hardhat
-  const { deploy } = deployments
-  const toWei = ethers.utils.parseEther
+  const {getNamedAccounts, deployments, ethers} = hardhat;
+  const {deploy} = deployments;
+  const toWei = ethers.utils.parseEther;
 
-  const { deployer, admin } = await getNamedAccounts()
-  const signer = await ethers.provider.getSigner(admin)
+  const {deployer, admin} = await getNamedAccounts();
+  const signer = await ethers.provider.getSigner(admin);
 
   // deploy erc721
   const prizeResult = await deploy("ERC721Mintable", {
-    args: [
-      "Upside Testnet Prize",
-      "UP-TP"
-    ],
-    contract: 'ERC721Mintable',
+    args: ["Upside Testnet Prize", "UP-TP"],
+    contract: "ERC721Mintable",
     from: deployer,
     skipIfAlreadyDeployed: false
-  })
+  });
 
   const prizeContract = await hardhat.ethers.getContractAt(
     "ERC721Mintable",
     prizeResult.address,
     signer
-  )
+  );
 
-  let tx = await prizeContract.mintFastMany(prizePoolContractAddress, numberOfPrizes)
+  let tx = await prizeContract.mintFastMany(
+    prizePoolContractAddress,
+    numberOfPrizes
+  );
 
   await ethers.provider.waitForTransaction(tx.hash);
-  const ERC721Mintable = await hardhat.artifacts.readArtifact('ERC721Mintable')
-  const erc721 = new ethers.utils.Interface(ERC721Mintable.abi)
-  const createResultReceipt = await ethers.provider.getTransactionReceipt(tx.hash)
-  const tokenIds = (createResultReceipt.logs.map(log => {
-    try {
-      l = erc721.parseLog(log)
-      if (l.name == "Minted")
-        return l.args.tokenId.toNumber()
-    } catch (e) {
-      return nulls
-    }
-  })).filter(x => x !== undefined)
+  const ERC721Mintable = await hardhat.artifacts.readArtifact("ERC721Mintable");
+  const erc721 = new ethers.utils.Interface(ERC721Mintable.abi);
+  const createResultReceipt = await ethers.provider.getTransactionReceipt(
+    tx.hash
+  );
+  const tokenIds = createResultReceipt.logs
+    .map(log => {
+      try {
+        l = erc721.parseLog(log);
+        if (l.name == "Minted") return l.args.tokenId.toNumber();
+      } catch (e) {
+        return nulls;
+      }
+    })
+    .filter(x => x !== undefined);
 
-  debug(`TokenIds minted: ${tokenIds}`)
+  debug(`TokenIds minted: ${tokenIds}`);
 
   return {
     erc721Address: prizeResult.address,
     tokenIds: tokenIds
-  }
+  };
 }
 
 module.exports = {
   addTestnetPrizes
-}
+};
---
+++ js/pool/addTestnetPrizeWrapper.js
@@ -0,0 +1,76 @@
+const hardhat = require("hardhat");
+const chalk = require("chalk");
+
+const {
+  getPrizePoolAddressFromBuilderTransaction
+} = require("../helpers/runPoolLifecycle");
+
+const debug = require("debug")("ptv3:deployTestnetPool");
+const {mintBank} = require("../helpers/mintBank");
+const {addTestnetPrizes} = require("../helpers/addTestnetPrizes");
+const {getEvents} = require("../../test/helpers/getEvents");
+
+function dim() {
+  console.log(chalk.dim.call(chalk, ...arguments));
+}
+
+function green() {
+  console.log(chalk.green.call(chalk, ...arguments));
+}
+
+async function main() {
+  const {getNamedAccounts, deployments, getChainId, ethers} = hardhat;
+  const toWei = ethers.utils.parseEther;
+  let {
+    deployer,
+    rng,
+    admin,
+    sablier,
+    reserveRegistry
+  } = await getNamedAccounts();
+
+  const signer = await ethers.provider.getSigner(admin);
+  debug(`Using deployer address: ${deployer}\n`);
+  debug(`Using admin address: ${admin}\n`);
+
+  const config = {
+    numberOfPrizes: 10,
+    prizePoolAddress: "0x29c4B18a595E5e78C7Bcd2aDcCE881F677FF2Ab7",
+    prizeStrategyAddress: "0x4C0216192e671e2E767236045067E48762Ec6c96"
+  };
+
+  const prizeStrategy = await hardhat.ethers.getContractAt(
+    "BanklessMultipleWinners",
+    config.prizeStrategyAddress,
+    signer
+  );
+
+  const res = await addTestnetPrizes(
+    config.prizePoolAddress,
+    config.numberOfPrizes
+  );
+
+  debug(`Add prizes w tokenIds ${res.tokenIds} to prizes strategy`);
+
+  // add prizes
+  let tx2 = await prizeStrategy.addPrizes(res.erc721Address, res.tokenIds);
+  await ethers.provider.waitForTransaction(tx2.hash);
+
+  let prizes = await prizeStrategy.currentPrizeAddresses();
+  console.log(prizes);
+  let tokenIds = await prizeStrategy.currentPrizeTokenIds(prizes[0]);
+
+  debug("Prizes: ", {
+    prizeAddress: prizes[0],
+    tokenIds: tokenIds
+  });
+
+  debug(`Done!`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
